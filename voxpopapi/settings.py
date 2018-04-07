66acf929c5bb4af833780c3911d29a341567fbd3
André Bedran <affbedran@gmail.com>
2018-04-07 17:52:32 -0300

    Revert "Usb01/cadastro de usuario"

--- voxpopapi/settings.py
+++ voxpopapi/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_swagger',
-    'api',
 ]
 
 MIDDLEWARE = [
@@ -69,11 +68,6 @@ TEMPLATES = [
         },
     },
 ]
-
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
-}
 
 WSGI_APPLICATION = 'voxpopapi.wsgi.application'
 
