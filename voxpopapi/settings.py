2180b8ede318e9ac0e63e820b19be1b3a6a5b007
Lucas362 <lucasvitorifg@gmail.com>
2018-04-18 20:31:17 -0300

    Added basicAuthentication and tokenAuthentication

--- voxpopapi/settings.py
+++ voxpopapi/settings.py
@@ -76,8 +76,12 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 10
 }
 
 WSGI_APPLICATION = 'voxpopapi.wsgi.application'
